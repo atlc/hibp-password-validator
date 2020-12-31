cf3ec3df2ae1d75a4eee1e6a8eb0b3e3879e0822
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-12-31 14:07:51 -0600

    Released v1.0.0

---
+++ index.js
@@ -0,0 +1,38 @@
+const crypto = require('crypto');
+const https = require('https');
+
+const validate = (password) => {
+    return new Promise((resolve, reject) => {
+
+        // Exposed plaintext passwords are hashed as SHA-1 per the maintainer of HIBP
+        const shasum = crypto.createHash('sha1');
+        shasum.update(password);
+        const hashedPass = shasum.digest('hex');
+
+        // Pass the API the first 5 characters of the resulting hash, and it returns a list of 
+        // ALL hashes that begin with those 5 characters, with those characters truncated
+        const req = https.get(`https://api.pwnedpasswords.com/range/${hashedPass.slice(0, 5)}`, async (res) => {
+            if (res.statusCode >= 300 || res.statusCode < 200) return reject(`Status code: ${res.statusCode}`);
+
+            let matchesList = [];
+            res.on('data', chunk => matchesList.push(chunk));
+            res.on('end', () => {
+                matchesList = matchesList.toString('utf-8');
+                const matchesArray = matchesList.split('\r\n');
+
+                const hashPrefixMatchString = hashedPass.slice(5, hashedPass.length).toUpperCase();
+                const matchIndex = matchesArray.findIndex(hash => hash.includes(hashPrefixMatchString));
+
+                if (matchIndex !== -1) {
+                    const matchInstances = Number(matchesArray[matchIndex].split(':')[1]);
+                    resolve({ isPwned: true, breaches: matchInstances });
+                } else {
+                    resolve({ isPwned: false, breaches: 0 });
+                }
+            })
+        })
+        .on('error', e => reject(e));
+    })
+}
+
+module.exports = validate;
