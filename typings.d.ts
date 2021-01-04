236d031e1c39bf4a4a34d6ce0c322da457cac94b
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-01-03 21:24:54 -0600

    Setting the Promise Rejection Error typing to any since they're that by default? Also provided example snippet for onHover in VSCode

--- typings.d.ts
+++ typings.d.ts
@@ -3,4 +3,14 @@ export type BreachDetails = {
     breaches: number;
 }
 
-export function validate(password: string): Promise<BreachDetails | PromiseRejectionEvent>;
+/**
+ * Returns an object with an `isPwned` boolean, and the number of breaches the password has been encountered in.
+ *
+ * Example:
+ * ```
+ * const test = await validate('hunter2');
+ * console.log(test);   // { isPwned: true, breaches: 17491 }
+ * ```
+ */
+export function validate(password: string): Promise<BreachDetails | any>;
+// Error objects are UNFORTUNATELY implicitly `any`, so hopefully this wraps up typings.
