9457f4192708fb4ef2e93da57724487f9c68bc55
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2021-01-03 21:49:28 -0600

    Another typing update

--- typings.d.ts
+++ typings.d.ts
@@ -1,16 +1,21 @@
-export type BreachDetails = {
-    isPwned: boolean;
-    breaches: number;
-}
+export = Validator;
 
-/**
- * Returns an object with an `isPwned` boolean, and the number of breaches the password has been encountered in.
- *
- * Example:
- * ```
- * const test = await validate('hunter2');
- * console.log(test);   // { isPwned: true, breaches: 17491 }
- * ```
- */
-export function validate(password: string): Promise<BreachDetails>;
-// Promise.reject errors are by default 'any'
+declare namespace Validator {
+    export type BreachDetails = {
+        isPwned: boolean;
+        breaches: number;
+    }
+
+    /**
+    * Returns an object with an `isPwned` boolean, and the number of breaches the password has been encountered in.
+    *
+    * Example:
+    * ```
+    * const test = await validate('hunter2');
+    * console.log(test);   // { isPwned: true, breaches: 17491 }
+    * ```
+    */
+    export function validate(password: string): Promise<BreachDetails | never>;
+    // Promise.reject errors are by default 'any'
+    // https://github.com/microsoft/TypeScript/issues/6283#issuecomment-240804072
+}
